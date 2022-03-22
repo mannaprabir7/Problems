6196a095c902448cf959ba5fb0e2a2267fa733b4
PRABIR MANNA <70742693+mannaprabir7@users.noreply.github.com>
2022-03-22 17:51:07 +0530

    Create smallest_string_with_given_numerical_value.cpp

---
+++ smallest_string_with_given_numerical_value.cpp
@@ -0,0 +1,42 @@
+class Solution {
+public:
+    string getSmallestString(int n, int k) {
+        string arr = "";
+ 
+    for(int i = 0; i < n; i++)
+        arr += 'a';
+ 
+    // Iteration from the last position
+    // in the array
+    for (int i = n - 1; i >= 0; i--)
+    {
+        k -= i;
+ 
+        // If k is a positive integer
+        if (k >= 0)
+        {
+ 
+            // 'z' needs to be inserted
+            if (k >= 26)
+            {
+                arr[i] = 'z';
+                k -= 26;
+            }
+ 
+            // Add the required character
+            else
+            {
+                char c= (char)(k + 97 - 1);
+                arr[i] = c;
+                k -= arr[i] - 'a' + 1;
+            }
+        }
+ 
+        else
+            break;
+ 
+        k += i;
+    }
+    return arr;
+    }
+};
